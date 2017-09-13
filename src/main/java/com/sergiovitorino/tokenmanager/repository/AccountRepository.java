13342aaadef3ca436473ed33382ecf39c7e0c15e
Sergio Vitorino <sergiovlvitorino@gmail.com>
2017-09-13 20:57:42 -0300

    Using MongoRepository

--- src/main/java/com/sergiovitorino/tokenmanager/repository/AccountRepository.java
+++ src/main/java/com/sergiovitorino/tokenmanager/repository/AccountRepository.java
@@ -2,11 +2,11 @@ package com.sergiovitorino.tokenmanager.repository;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.sergiovitorino.tokenmanager.model.Account;
 
-public interface AccountRepository extends CrudRepository<Account, String>{
+public interface AccountRepository extends MongoRepository<Account, String>{
 
 	List<Account> findByDestroyedAtIsNull();
 
